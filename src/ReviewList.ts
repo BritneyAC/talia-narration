2352dc368ac28993dab88d19b6a4fe5936f8201f
unknown <britneyartemis@gmail.com>
2022-06-20 20:39:11 -0700

    added real reviews

--- src/ReviewList.ts
+++ src/ReviewList.ts
@@ -1,40 +1,50 @@
 const reviewList = [
   {
     id : 1,
-    name : "Bob",
-    review : "she is good",
+    book : "Of Heart and Wings",
+    name : "Lauren",
+    review : `
+      Wow, I am just amazed by how much I enjoyed Egan and Raiden brought to life. The voice fit perfectly and being able to visualize this book while someone else narrated it was just the icing on the cake!`,
     rating : 5
   },
   {
-    id : 1,
-    name : "Bob",
-    review : "she is good",
+    id : 2,
+    book : "Of Heart and Wings",
+    name : "Susie",
+    review : "I have been discovering some great new narrators lately and this is one of them. I thought all the emotions were right there grabbing me and pulling me into the story as was the humor.",
     rating : 5
   },
   {
-    id : 1,
-    name : "Bob",
-    review : "she is good",
+    id : 3,
+    book : "Of Heart and Wings",
+    name : "B. M. Stauffer",
+    review : `
+      What a beautiful story told by such a wonderful narrator! Not only did McQueen create a wholesome tale of shape shifting into dragons, princes, and LGBT romance, but the narrator brought the entire story to life with his beautiful narration of Raiden and Egan!
+
+      Worth every wholesome second! `,
     rating : 5
   },
   {
-    id : 1,
-    name : "Bob",
-    review : "she is good",
+    id : 4,
+    book : "Blind Alpine",
+    name : "Patricia",
+    review : "Talia Carver is our narrator and she is great! This is my first listen by her and she has the voices to fit each character and an easy pace to understand. I enjoyed her performance and will be watching for more from her.",
+    rating : 4
+  },
+  {
+    id : 5,
+    book : "Bound in Fire",
+    name : "Rachel",
+    review : "The narrator as always, performed an exemplary audio delivery and narration.",
     rating : 5
   },
   {
-    id : 1,
-    name : "Bob",
-    review : "she is good",
-    rating : 5
+    id : 6,
+    book : "Bound in Fire",
+    name : "Abrianna",
+    review : "The way the narrator portrays them is perfect. I didn't feel lost because the narration was spot on in many instances",
+    rating : 4
   },
-  {
-  id : 2,
-    name : "Jen",
-    review : "She did a most poggers job reading out this book although I did not like the book I very much enjoyed her preformanceShe did a most poggers job reading out this book although I did not like the book I very much enjoyed her preformanceShe did a most poggers job reading out this book although I did not like the book I very much enjoyed her preformanceShe did a most poggers job reading out this book although I did not like the book I very much enjoyed her preformance",
-    rating: 5
-  }
 ]
 
 export default reviewList
